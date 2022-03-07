5699dd1023b1f49becf92f9e0f7322e236372154
Isabelle Sicotte <erebiel@gmail.com>
2022-03-07 15:45:20 -0500

    Start styling searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,15 +9,24 @@ export default function SearchBar(props) {
         boxShadow: '0px 16px 30px -10px rgba(70, 96, 187, 0.2)'
       }
 
+      const inputstyles = props.darkMode ? {
+          color: '#FFFFFF'
+      } : {
+          color: '#4B6A9B'
+      }
+
     return(
         <div className="searchbar" style={styles}>
+          <img src="./assets/icon-search.svg" alt="Search icon" />
           <input
             type="text"
             name="username"
             id="username"
             placeholder="Search GitHub username..."
+            className="search--input"
+            style={inputstyles}
           />
-          <button>Search</button>
+          <button className="search--btn">Search</button>
         </div>
     )
 }
