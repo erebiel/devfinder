a5f3842db0b4aa7a579d91a085b121d1e37d9f38
Isabelle Sicotte <erebiel@gmail.com>
2022-03-11 13:40:01 -0500

    Add main static content and start styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,49 @@ export default function Main(props) {
       }
 
     return (
-        <main style={styles}>INFO</main>
+        <main style={styles}>
+            <div>
+                <img src="./assets/defaulticon.png" alt="The Octocat" className="user--icon"/>
+            </div>
+            <div className="main--content">
+                <div className="content--header">
+                    <div>
+                        <h3>The Octocat</h3>
+                        <a href="#">@octocat</a>
+                        <p>This profile has no bio</p>
+                    </div>
+                    <div>Joined 25 Jan 2011</div> 
+                </div>
+                
+                <div className="stats">
+                    <div className="stats--stat">
+                        <p>Repos</p>
+                        <p className="stats--nb">8</p>
+                    </div>
+                    <div className="stats--stat">
+                        <p>Followers</p>
+                        <p className="stats--nb">3938</p>
+                    </div>
+                    <div className="stats--stat">
+                        <p>Following</p>
+                        <p className="stats--nb">9</p>
+                    </div>
+                </div>
+                <div className="infos">
+                    <div className="infos--info">
+                        <img src="./assets/icon-location.svg" alt="Location" /> San Francisco
+                    </div>
+                    <div className="infos--info">
+                        <img src="./assets/icon-twitter.svg" alt="Twitter" /> Not Available
+                    </div>
+                    <div className="infos--info">
+                        <img src="./assets/icon-website.svg" alt="Website" /> https://github.blog
+                    </div>
+                    <div className="infos--info">
+                        <img src="./assets/icon-company.svg" alt="Company" /> @github
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
