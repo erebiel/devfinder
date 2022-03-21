41dae7accb05a5336740eec07b09d72a58985e78
Isabelle Sicotte <erebiel@gmail.com>
2022-03-21 09:27:50 -0400

    Add text styles and semantics

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(props) {
     return(
         <header>
-          <h3 className="header--title">devfinder</h3>
+          <h1>devfinder</h1>
           <button className="header--mode" onClick={props.toggleDarkMode}>
             {props.darkMode ? "Light" : "Dark"} {props.darkMode ? <img src='./assets/icon-sun.svg'/> : <img src='./assets/icon-moon.svg'/>}
           </button>
